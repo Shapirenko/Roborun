44445675dad56c3573e29e7ba2d7a90d4d7c82dc
Shapirenko <158288545+Shapirenko@users.noreply.github.com>
2024-03-10 23:09:42 +0200

    Fixed some assets

--- game.js
+++ game.js
@@ -61,19 +61,19 @@ function create() {
     for (var x = 0; x <= worldWidth; x = x + Phaser.Math.Between(200, 800)) {
         objects
             .create(x, 1050, 'screen')
-            .setScale(Phaser.Math.FloatBetween(0.5, 2,))
+            .setScale(Phaser.Math.FloatBetween(0.8, 1.7,))
             .setDepth(Phaser.Math.Between(0, 2))
             .setOrigin(0, 1)
             .refreshBody();
         objects
             .create(x = x + Phaser.Math.Between(50, 200), 1050, 'box')
-            .setScale(Phaser.Math.FloatBetween(0.5, 2,))
+            .setScale(Phaser.Math.FloatBetween(0.5, 1.2,))
             .setDepth(Phaser.Math.Between(0, 2))
             .setOrigin(0, 1)
             .refreshBody();
         objects
             .create(x = x + Phaser.Math.Between(45, 300), 1050, 'barrel')
-            .setScale(Phaser.Math.FloatBetween(0.5, 2,))
+            .setScale(Phaser.Math.FloatBetween(0.5, 1.2,))
             .setDepth(Phaser.Math.Between(0, 2))
             .setOrigin(0, 1)
             .refreshBody();
